4a7f9d405479d7832ef864fe51c45f94dbbd0120
Adam Pridmore <adam_pridmore@hotmail.com>
2018-11-21 21:37:53 +0000

    Replace map & contains with forall

--- src/main/scala/all_inclusive/Rotations.scala
+++ src/main/scala/all_inclusive/Rotations.scala
@@ -1,13 +1,14 @@
 package all_inclusive
 
 object Rotations {
+
   def rotate(str: String, howManyToRotate: Int): String = {
     str.substring(howManyToRotate,str.length) + str.substring(0, howManyToRotate)
   }
 
   def containAllRots(strng: String, arr: List[String]): java.lang.Boolean = {
-    !(for(i <- 0 until strng.length) yield {rotate(strng, i)})
-        .map(rotation => arr.contains(rotation))
-        .contains(false)
+
+    (for (i <- 0 until strng.length) yield rotate(strng, i))
+      .forall(rotation => arr.contains(rotation))
   }
 }
