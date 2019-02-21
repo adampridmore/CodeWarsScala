0ae5d52c31df2e4b1ef91b55c7eb9d7d061ed213
Adam Pridmore <adam_pridmore@hotmail.com>
2019-02-21 21:53:11 +0000

    - Refactored to use a BigDecimal range.

--- src/test/scala/sumoffirstnterms/sumOfFirstNTerms.scala
+++ src/test/scala/sumoffirstnterms/sumOfFirstNTerms.scala
@@ -1,13 +1,15 @@
 package sumoffirstnterms
 
 import org.scalatest._
+import scala.BigDecimal._
+import scala.collection.immutable.Range.BigDecimal._
 
 // From: https://www.codewars.com/kata/555eded1ad94b00403000071/train/scala
 class sumOfFirstNTerms extends FlatSpec with Matchers {
 
   def seriesSum(n: Int): String = {
     val sum =
-      (for (i <- 1.0 to n.toDouble by 1.0) yield i)
+      (for (i <- inclusive(1.0, valueOf(n), 1.0)) yield i)
         .map(x => 1.0 + (x - 1.0) * 3.0)
         .map(x => 1 / x)
         .sum
